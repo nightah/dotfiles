59ad692956bc86a4d770a1246e5e20c80ec670cc
Shinryuu <timo.taipaleenmaki@gmail.com>
2016-04-09 16:15:40 +0300

    meow

--- .bin/lemonclock.sh
+++ .bin/lemonclock.sh
@@ -5,13 +5,17 @@
 # settings
 BG="#1D1F21"
 FG="#A8A8A8"
-RES="160x24+3016+20"
+#RES="160x24+3016+20"
+RES="160x24+1740+20"
+BRD="2"
+BRDC="#303030"
 FONT="-*-cure.se-medium-r-*-*-11-*-*-*-*-*-*-*" 
 
 # clock
 while :; do
 	echo "%{c}$(date "+%b %d %A %H:%M")%{c}"
-	sleep 45s
+	sleep 50s
 done |
 
-lemonbar -b -g "${RES}" -f "${FONT}" -B "${BG}" -F "${FG}"
+lemonbar -b -g "${RES}" -r "${BRD}" -R "${BRDC}" -f "${FONT}" -B "${BG}" -F "${FG}"
+
